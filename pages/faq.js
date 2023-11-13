27f8118ff39109a0a3cc9beb6e003d3d600dd33a
André Almeida <andrefelipi2@hotmail.com>
2023-11-12 22:00:20 -0300

    finallizado

--- pages/faq.js
+++ pages/faq.js
@@ -1,12 +1,23 @@
-import Link from '../src/components/Link';
+import FAQScreen from '../src/screens/FAQScreen/FAQScreen';
 
-export default function FAQpage(){
-    return(
-        <div>
-            <h1>Alura Cases - Página de Perguntas FAQ</h1>
-            <Link href="/">
-                Ir para a home page
-            </Link>
-        </div>
-    )
-}
+export default FAQScreen; 
+
+export async function getStaticProps() {
+    console.log('Em modo DEV, sempre roda! A cada acesso')
+    console.log('Roda SOMENTE em build time')
+    const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
+    const faq = await fetch(FAQ_API_URL)
+        .then((respostaDoServidor) => {
+            return respostaDoServidor.json();
+        })
+        .then((resposta) => {
+            return resposta;
+        });
+
+    return {
+      props: {
+          qualquercoisa: 'que eu passar aqui',
+          faq,
+      },
+    };
+}  
